1b80f2869a1751337e876dfab58dbc8f9236df6e
fidoriel <49869342+fidoriel@users.noreply.github.com>
2024-11-05 16:12:02 +0100

    split refresh lib

--- backend/parse_library.rs
+++ backend/parse_library.rs
@@ -14,8 +14,8 @@ use diesel_async::AsyncConnection;
 use diesel_async::RunQueryDsl;
 use serde::{Deserialize, Serialize};
 use std::collections::HashSet;
-use std::panic;
 use std::path::{Path, PathBuf};
+use std::{any, panic};
 use tokio::fs;
 use tracing::{debug, error, info};
 use uuid::Uuid;
@@ -140,61 +140,47 @@ pub async fn refresh_library(
     // add new or update models
 
     for dir in data_dirs {
-        let model_pack_meta = get_modelpack_meta(&dir).await.unwrap();
-
-        let relative_dir = pathdiff::diff_paths(&dir, &config.libraries_path).unwrap();
-
-        let result: Option<Model3D> = models3d::dsl::models3d
-            .filter(models3d::dsl::folder_path.eq(relative_dir.to_str().unwrap()))
-            .first::<Model3D>(&mut connection)
-            .await
-            .ok();
-
-        let mut image_dir = dir.clone();
-        image_dir.push("images");
-
-        let new_object = NewModel3D::from_model_pack_v0_1(
-            &model_pack_meta,
-            &relative_dir,
-            get_all_image_files(&image_dir, &dir).await.unwrap(),
-        )
-        .unwrap();
-
-        if let Some(existing_model) = result {
-            diesel::update(models3d::dsl::models3d.find(existing_model.id))
-                .set((
-                    models3d::dsl::title.eq(&new_object.title),
-                    models3d::dsl::name.eq(&new_object.name),
-                    models3d::dsl::license.eq(&new_object.license),
-                    models3d::dsl::author.eq(&new_object.author),
-                    models3d::dsl::origin.eq(&new_object.origin),
-                    models3d::dsl::images.eq(new_object.images),
-                ))
-                .execute(&mut connection)
-                .await
-                .unwrap();
-            debug!("Updated {:?}", new_object.folder_path)
-        } else {
-            diesel::insert_into(models3d::table)
-                .values(&new_object)
-                .execute(&mut connection)
-                .await
-                .unwrap();
-            debug!("Created Model {:?}", new_object.folder_path)
-        }
+        add_or_update_model(&config, &mut connection, &dir).await?;
     }
 
     // generate file3d entrys
     fs::create_dir_all(config.preview_cache_dir.clone()).await?;
 
+    clean_file_system(&config, &mut connection).await?;
+
+    let models = models3d::dsl::models3d
+        .load::<Model3D>(&mut connection)
+        .await
+        .unwrap();
+
+    // add refresh files in model folders
+    for model in models {
+        render_preview(&config, &mut connection, model).await?;
+    }
+
+    // delete old cache images
+    clean_cache(config, &mut connection).await?;
+
+    info!(
+        "Finished lib scan at {}",
+        Local::now().format("%Y-%m-%d %H:%M:%S")
+    );
+
+    Ok(())
+}
+
+pub async fn clean_file_system<Conn>(config: &Config, connection: &mut Conn) -> anyhow::Result<()>
+where
+    Conn: AsyncConnection<Backend = diesel::sqlite::Sqlite>,
+{
     let files = files3d::dsl::files3d
-        .load::<File3D>(&mut connection)
+        .load::<File3D>(connection)
         .await
         .unwrap();
 
     // delete file references deleted in fs
     for file in files {
-        let file_pth = file.get_file_path(&mut connection, &config).await.clone();
+        let file_pth = file.get_file_path(connection, &config).await.clone();
 
         if fs::metadata(&file_pth).await.is_ok() {
             let current_sha = sha256::try_async_digest(file_pth.clone())
@@ -207,111 +193,158 @@ pub async fn refresh_library(
             }
         }
         diesel::delete(files3d::dsl::files3d.filter(files3d::dsl::id.eq(file.id)))
-            .execute(&mut connection)
+            .execute(connection)
             .await
             .unwrap();
         debug!("Deleted File Reference from DB {:?}", file.file_path)
     }
+    anyhow::Ok(())
+}
 
-    let models = models3d::dsl::models3d
-        .load::<Model3D>(&mut connection)
+pub async fn add_or_update_model<Conn>(
+    config: &Config,
+    connection: &mut Conn,
+    dir: &PathBuf,
+) -> anyhow::Result<()>
+where
+    Conn: AsyncConnection<Backend = diesel::sqlite::Sqlite>,
+{
+    let model_pack_meta = get_modelpack_meta(&dir).await.unwrap();
+
+    let relative_dir = pathdiff::diff_paths(&dir, &config.libraries_path).unwrap();
+
+    let result: Option<Model3D> = models3d::dsl::models3d
+        .filter(models3d::dsl::folder_path.eq(relative_dir.to_str().unwrap()))
+        .first::<Model3D>(connection)
         .await
-        .unwrap();
+        .ok();
 
-    // add refresh files in model folders
-    for model in models {
-        let mut model_base_path = config.libraries_path.clone();
-        model_base_path.push(model.folder_path);
+    let mut image_dir = dir.clone();
+    image_dir.push("images");
 
-        let mut model_file_base_path = model_base_path.clone();
-        model_file_base_path.push("files");
+    let new_object = NewModel3D::from_model_pack_v0_1(
+        &model_pack_meta,
+        &relative_dir,
+        get_all_image_files(&image_dir, &dir).await.unwrap(),
+    )
+    .unwrap();
 
-        debug!("starting search for {:?}", model_file_base_path);
-        for entry in walkdir::WalkDir::new(model_file_base_path) {
-            let mesh_files = ["stl", "3mf", "obj"];
-            let entry: walkdir::DirEntry = entry.unwrap();
-            let file_pth = entry.path();
+    if let Some(existing_model) = result {
+        diesel::update(models3d::dsl::models3d.find(existing_model.id))
+            .set((
+                models3d::dsl::title.eq(&new_object.title),
+                models3d::dsl::name.eq(&new_object.name),
+                models3d::dsl::license.eq(&new_object.license),
+                models3d::dsl::author.eq(&new_object.author),
+                models3d::dsl::origin.eq(&new_object.origin),
+                models3d::dsl::images.eq(new_object.images),
+            ))
+            .execute(connection)
+            .await
+            .unwrap();
+        debug!("Updated {:?}", new_object.folder_path)
+    } else {
+        diesel::insert_into(models3d::table)
+            .values(&new_object)
+            .execute(connection)
+            .await
+            .unwrap();
+        debug!("Created Model {:?}", new_object.folder_path)
+    }
+    anyhow::Ok(())
+}
 
-            if file_pth.is_dir() {
+pub async fn render_preview<Conn>(
+    config: &Config,
+    connection: &mut Conn,
+    model: Model3D,
+) -> anyhow::Result<()>
+where
+    Conn: AsyncConnection<Backend = diesel::sqlite::Sqlite>,
+{
+    let mut model_base_path = config.libraries_path.clone();
+    model_base_path.push(model.folder_path);
+
+    debug!("starting search for {:?}", model_base_path);
+    for entry in walkdir::WalkDir::new(&model_base_path) {
+        let entry = match entry {
+            Ok(path) => path,
+            Err(e) => return Err(e.into()),
+        };
+
+        let mesh_files = ["stl", "3mf", "obj"];
+        let file_pth = entry.path();
+
+        if file_pth.is_dir() {
+            continue;
+        }
+
+        if let Some(extension) = file_pth.extension() {
+            if !mesh_files.contains(&extension.to_str().unwrap()) {
                 continue;
             }
+        }
 
-            if let Some(extension) = file_pth.extension() {
-                if !mesh_files.contains(&extension.to_str().unwrap()) {
-                    continue;
-                }
-            }
-
-            let result: Option<File3D> = files3d::dsl::files3d
-                .filter(
-                    files3d::dsl::file_path.eq(pathdiff::diff_paths(file_pth, &model_base_path)
-                        .unwrap()
-                        .to_str()
-                        .unwrap()),
-                )
-                .first::<File3D>(&mut connection)
-                .await
-                .ok();
-
-            // entry exist and everything is fine
-            if result.is_some() {
-                continue;
-            }
-
-            let mut img_path = config.preview_cache_dir.clone();
-            let file_name = format!("{}.png", Uuid::new_v4());
-            img_path.push(file_name.clone());
-
-            let mut render_config = stl_thumb::config::Config::default();
-            render_config.model_filename = file_pth.to_str().unwrap().to_string();
-            render_config.img_filename = img_path.to_str().unwrap().to_string();
-
-            let preview_image = if let Err(err) = panic::catch_unwind(|| {
-                stl_thumb::render_to_file(&render_config).unwrap();
-            }) {
-                error!(
-                    "Unable to render preview: {:?} Error: {:?}",
-                    file_pth.to_str(),
-                    err
-                );
-                None
-            } else {
-                Some(file_name)
-            };
-
-            let new_file = NewFile3D {
-                model_id: model.id,
-                file_path: pathdiff::diff_paths(file_pth, &model_base_path)
+        let result: Option<File3D> = files3d::dsl::files3d
+            .filter(
+                files3d::dsl::file_path.eq(pathdiff::diff_paths(file_pth, &model_base_path)
                     .unwrap()
                     .to_str()
+                    .unwrap()),
+            )
+            .first::<File3D>(connection)
+            .await
+            .ok();
+
+        // entry exist and everything is fine
+        if result.is_some() {
+            continue;
+        }
+
+        let mut img_path = config.preview_cache_dir.clone();
+        let file_name = format!("{}.png", Uuid::new_v4());
+        img_path.push(file_name.clone());
+
+        let mut render_config = stl_thumb::config::Config::default();
+        render_config.model_filename = file_pth.to_str().unwrap().to_string();
+        render_config.img_filename = img_path.to_str().unwrap().to_string();
+
+        let preview_image = if let Err(err) = panic::catch_unwind(|| {
+            stl_thumb::render_to_file(&render_config).unwrap();
+        }) {
+            error!(
+                "Unable to render preview: {:?} Error: {:?}",
+                file_pth.to_str(),
+                err
+            );
+            None
+        } else {
+            Some(file_name)
+        };
+
+        let new_file = NewFile3D {
+            model_id: model.id,
+            file_path: pathdiff::diff_paths(file_pth, &model_base_path)
+                .unwrap()
+                .to_str()
+                .unwrap()
+                .to_string(),
+            preview_image,
+            file_hash: Some(
+                sha256::try_async_digest(file_pth)
+                    .await
                     .unwrap()
                     .to_string(),
-                preview_image,
-                file_hash: Some(
-                    sha256::try_async_digest(file_pth)
-                        .await
-                        .unwrap()
-                        .to_string(),
-                ),
-            };
-            diesel::insert_into(files3d::table)
-                .values(&new_file)
-                .execute(&mut connection)
-                .await
-                .unwrap();
-            debug!("Created File {:?}", new_file.file_path)
-        }
+            ),
+        };
+        diesel::insert_into(files3d::table)
+            .values(&new_file)
+            .execute(connection)
+            .await
+            .unwrap();
+        debug!("Created File {:?}", new_file.file_path)
     }
-
-    // delete old cache images
-    clean_cache(config, &mut connection).await?;
-
-    info!(
-        "Finished lib scan at {}",
-        Local::now().format("%Y-%m-%d %H:%M:%S")
-    );
-
-    Ok(())
+    anyhow::Ok(())
 }
 
 pub async fn clean_cache<Conn>(config: Config, connection: &mut Conn) -> anyhow::Result<()>
